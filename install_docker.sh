3a414033e1e8853045cefa01bf568b08d3c45c44
Jonathan Pettersson <jonathan.anders.pettersson@gmail.com>
2014-03-09 18:22:45 +0100

    Configure docker to run without sudo

--- install_docker.sh
+++ install_docker.sh
@@ -5,3 +5,15 @@ sudo sh -c "echo deb http://get.docker.io/ubuntu docker main\
 > /etc/apt/sources.list.d/docker.list"
 sudo apt-get update
 sudo apt-get install -y lxc-docker
+
+# Add the docker group if it doesn't already exist.
+sudo groupadd docker
+
+# Add the connected user "${USER}" to the docker group.
+# Change the user name to match your preferred user.
+# You may have to logout and log back in again for
+# this to take effect.
+sudo gpasswd -a vagrant docker
+
+# Restart the Docker daemon.
+sudo service docker restart
